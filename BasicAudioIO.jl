c48d9190929f2a74e511b9d87a00bdca9da738be
Lakebak <nicola.corbellini@edu.unige.it>
2022-03-30 23:39:57 +0200

    read audio

--- BasicAudioIO.jl
+++ BasicAudioIO.jl
@@ -1,2 +1,12 @@
 using WAV
 
+function read_audio(path::String, subrange=:, format="double"::String)
+
+    signal, sr = wavread(path, subrange, format)
+
+    return signal, sr
+end
+
+function to_mono()
+    
+end
